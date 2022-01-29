af5abf5dc9ee0f70626176e8ea91a1f030602aab
Igor <leonidlee777@gmail.com>
2022-01-29 09:15:32 +0200

    add Redux toolkit

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { useEffect } from "react";
 import classNames from 'classnames';
-import { heroesActiveFiltered, filtersFetching, filtersFetched, filtersFetchingError} from '../../actions';
+import { heroesActiveFiltered, fetchFilters} from '../../actions';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Spinner from "../spinner/Spinner";
@@ -23,11 +23,7 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
-
+        dispatch(fetchFilters(request));
         // eslint-disable-next-line
     }, []);
 
